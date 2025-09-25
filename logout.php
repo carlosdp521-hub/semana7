633edcd8c63bd858d6cbd6b0476668507fcd3fd0
carlosdp521-hub <carlosdp521@gmail.com>
2025-09-25 02:37:16 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.php - Script para cerrar la sesión del usuario.
-session_start();
-session_unset();
-session_destroy();
-header("Location: acceso_administrativo.php"); // Redirige al formulario de login.
-exit();
-?>
